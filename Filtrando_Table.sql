2512a24ec582839ea12c200e7515374789950e57
Guilherme Lavezzo <65193596+Godofin@users.noreply.github.com>
2021-12-31 15:55:32 -0300

    Delete Filtrando_Table.sql

--- Filtrando_Table.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM tbproduto WHERE PRODUTO = '544931';
-
-SELECT * FROM tbcliente WHERE CIDADE = 'Rio de Janeiro';
-
-SELECT * FROM tbproduto WHERE SABOR = 'CITRICOS';
-
-UPDATE tbproduto SET SABOR = 'Citricos' WHERE SABOR = 'LIMÃO';
